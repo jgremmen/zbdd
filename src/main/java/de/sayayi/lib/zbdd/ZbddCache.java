33efd3ef15e0719883e3864c539a2403de2fdbb8
jeroen.gremmen <jeroen.gremmen@sayayi.de>
2021-11-19 11:16:54 +0100

    add Apache 2.0 license

--- src/main/java/de/sayayi/lib/zbdd/ZbddCache.java
+++ src/main/java/de/sayayi/lib/zbdd/ZbddCache.java
@@ -1,16 +1,43 @@
+/*
+ * Copyright 2021 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.sayayi.lib.zbdd;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
 
 import java.util.function.IntSupplier;
 
 
+/**
+ * @author Jeroen Gremmen
+ */
 public interface ZbddCache
 {
-  int lookupOrPutIfAbsent(@NotNull UnaryOperation operation, int zbdd, int var, @NotNull IntSupplier resultSupplier);
+  @Range(from = 0, to = Zbdd.MAX_NODES)
+  int lookupOrPutIfAbsent(@NotNull UnaryOperation operation,
+                          @Range(from = 0, to = Zbdd.MAX_NODES) int zbdd,
+                          @Range(from = 1, to = Integer.MAX_VALUE) int var,
+                          @NotNull IntSupplier resultSupplier);
 
 
-  int lookupOrPutIfAbsent(@NotNull BinaryOperation operation, int p, int q, @NotNull IntSupplier resultSupplier);
+  @Range(from = 0, to = Zbdd.MAX_NODES)
+  int lookupOrPutIfAbsent(@NotNull BinaryOperation operation,
+                          @Range(from = 0, to = Zbdd.MAX_NODES) int p,
+                          @Range(from = 0, to = Zbdd.MAX_NODES) int q,
+                          @NotNull IntSupplier resultSupplier);
 
 
   void clear();
--- src/main/java/de/sayayi/lib/zbdd/ZbddNameResolver.java
+++ src/main/java/de/sayayi/lib/zbdd/ZbddNameResolver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Jeroen Gremmen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.sayayi.lib.zbdd;
 
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +24,9 @@ import static java.lang.Integer.MAX_VALUE;
 import static java.util.stream.Collectors.joining;
 
 
+/**
+ * @author Jeroen Gremmen
+ */
 public interface ZbddNameResolver
 {
   default @NotNull String getEmptyName() {
