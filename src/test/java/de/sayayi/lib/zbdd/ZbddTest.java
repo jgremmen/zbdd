9b4dc4fb3b9858db6418d4d8a7d6bdb325a6d439
jeroen.gremmen <jeroen.gremmen@sayayi.de>
2021-11-25 11:35:19 +0100

    code cleanup

--- src/test/java/de/sayayi/lib/zbdd/ZbddTest.java
+++ src/test/java/de/sayayi/lib/zbdd/ZbddTest.java
@@ -151,7 +151,7 @@ class ZbddTest
   }
 
 
-  @Test void queens01() { checkSolution(1, 1,16); }
+  @Test void queens01() { checkSolution(1, 1, 16); }
   @Test void queens02() { checkSolution(2, 0, 16); }
   @Test void queens03() { checkSolution(3, 0, 16); }
   @Test void queens04() { checkSolution(4, 2, 32); }
@@ -243,7 +243,7 @@ class ZbddTest
 
 
     @Override
-    public @Range(from = 4, to = MAX_NODES) int getInitialCapacity() {
+    public int getInitialCapacity() {
       return initialSize;
     }
 
