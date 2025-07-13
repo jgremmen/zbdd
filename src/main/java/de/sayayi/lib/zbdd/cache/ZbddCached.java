f583ee37e08ac3f31f0bad0478b2a3176eee71ba
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2025-07-13 16:23:29 +0200

    chore(license): Fix copyright year for `ZbddCached`

--- src/main/java/de/sayayi/lib/zbdd/cache/ZbddCached.java
+++ src/main/java/de/sayayi/lib/zbdd/cache/ZbddCached.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Jeroen Gremmen
+ * Copyright 2025 Jeroen Gremmen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
